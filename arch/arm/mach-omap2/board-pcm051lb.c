d1ade8885631dc73288baad71155942f3931aa5e
Nitesh Chowdhary <nitesh.c@phytec.in>
2013-10-11 16:40:59 +0530

    PCM051LB: Initial board support

---
+++ arch/arm/mach-omap2/board-pcm051lb.c
@@ -0,0 +1,175 @@
+/*
+ Board support file for Phytec Cosmic-am335x.
+
+ Copyright (C) 2013 Phytec Embedded Pvt. Ltd.
+
+ Based on mach-omap2/board-am335xevm.c
+
+ Copyright (C) 2011 Texas Instruments, Inc. - http://www.ti.com/
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License as
+ published by the Free Software Foundation version 2.
+
+ This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ kind, whether express or implied; without even the implied warranty
+ of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+*/
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/input.h>
+#include <linux/err.h>
+#include <linux/export.h>
+#include <linux/reboot.h>
+#include <linux/clk.h>
+#include <linux/opp.h>
+#include <linux/platform_device.h>
+
+#include <mach/hardware.h>
+
+#include <asm/mach-types.h>
+#include <asm/mach/arch.h>
+#include <asm/mach/map.h>
+#include <asm/hardware/asp.h>
+
+#include <plat/omap_device.h>
+#include <plat/irqs.h>
+#include <plat/board.h>
+#include <plat/common.h>
+#include <plat/mmc.h>
+
+#include "cpuidle33xx.h"
+#include "mux.h"
+#include "hsmmc.h"
+#include "common.h"
+#include "am33xx_generic.h"
+
+/* convert GPIO signal to GPIO pin number */
+#define GPIO_TO_PIN(bank, gpio) (32 * (bank) + (gpio))
+
+/* module pin mux structure */
+struct pinmux_config {
+	const char *string_name; /* signal name format */
+	int val; /* options for the mux register value */
+};
+
+/*
+* @pin_mux - single module pin-mux structure which defines
+*	pin-mux details for all its pins
+*/
+static void setup_pin_mux(struct pinmux_config *pin_mux)
+{
+	int i;
+
+	for (i = 0; pin_mux->string_name != NULL; pin_mux++)
+		omap_mux_init_signal(pin_mux->string_name, pin_mux->val);
+}
+
+/* pin-mux for mmc0 */
+static struct pinmux_config mmc0_pin_mux[] = {
+	{"mmc0_dat3.mmc0_dat3", OMAP_MUX_MODE0 | AM33XX_PIN_INPUT_PULLUP},
+	{"mmc0_dat2.mmc0_dat2", OMAP_MUX_MODE0 | AM33XX_PIN_INPUT_PULLUP},
+	{"mmc0_dat1.mmc0_dat1", OMAP_MUX_MODE0 | AM33XX_PIN_INPUT_PULLUP},
+	{"mmc0_dat0.mmc0_dat0", OMAP_MUX_MODE0 | AM33XX_PIN_INPUT_PULLUP},
+	{"mmc0_clk.mmc0_clk",	OMAP_MUX_MODE0 | AM33XX_PIN_INPUT_PULLUP},
+	{"mmc0_cmd.mmc0_cmd",	OMAP_MUX_MODE0 | AM33XX_PIN_INPUT_PULLUP},
+	{"spi0_cs1.mmc0_sdcd",	OMAP_MUX_MODE7 | AM33XX_PIN_INPUT_PULLUP},
+	{NULL, 0},
+};
+
+/* mmc0 platform data */
+static struct omap2_hsmmc_info am335x_mmc[] __initdata	= {
+	{
+		.mmc		= 1,
+		.caps		= MMC_CAP_4_BIT_DATA,
+		.gpio_cd	= GPIO_TO_PIN(0, 6),
+		.gpio_wp	= -EINVAL,
+		.ocr_mask	= MMC_VDD_32_33 | MMC_VDD_33_34, /* 3V3 */
+	},
+	{
+		.mmc		= 0,    /* will be set at runtime */
+	},
+	{
+		.mmc		= 0,    /* will be set at runtime */
+	},
+	{}	/* Terminator */
+};
+
+/* mmc0 initialization */
+static void mmc0_init(void)
+{
+	setup_pin_mux(mmc0_pin_mux);
+
+	omap2_hsmmc_init(am335x_mmc);
+
+	return;
+}
+
+static struct resource am33xx_cpuidle_resources[] = {
+	{
+		.start		= AM33XX_EMIF0_BASE,
+		.end		= AM33XX_EMIF0_BASE + SZ_32K - 1,
+		.flags		= IORESOURCE_MEM,
+	},
+};
+
+/* AM33XX devices support DDR2 power down */
+static struct am33xx_cpuidle_config am33xx_cpuidle_pdata = {
+	.ddr2_pdown = 1,
+};
+
+static struct platform_device am33xx_cpuidle_device = {
+	.name			= "cpuidle-am33xx",
+	.num_resources		= ARRAY_SIZE(am33xx_cpuidle_resources),
+	.resource		= am33xx_cpuidle_resources,
+	.dev = {
+		.platform_data	= &am33xx_cpuidle_pdata,
+	},
+};
+
+static void __init am33xx_cpuidle_init(void)
+{
+	int ret;
+
+	am33xx_cpuidle_pdata.emif_base = am33xx_get_mem_ctlr();
+
+	ret = platform_device_register(&am33xx_cpuidle_device);
+
+	if (ret)
+		pr_warning("AM33XX cpuidle registration failed\n");
+}
+
+/* muxing initialization */
+static void pcm051lb_mux_init(void)
+{
+	mmc0_init();
+}
+
+static void __init pcm051lb_init(void)
+{
+	am33xx_cpuidle_init();
+	pcm051lb_mux_init();
+	omap_serial_init();
+	/* SDRAM controller initialization */
+	omap_sdrc_init(NULL, NULL);
+}
+
+static void __init pcm051lb_map_io(void)
+{
+	omap2_set_globals_am33xx();
+	omapam33xx_map_common_io();
+}
+
+MACHINE_START(PCM051LB, "pcm051lb")
+	/* Maintainer: PHYTEC India */
+	.atag_offset	= 0x100,
+	.map_io		= pcm051lb_map_io,
+	.init_early	= am33xx_init_early,
+	.init_irq	= ti81xx_init_irq,
+	.handle_irq	= omap3_intc_handle_irq,
+	.timer		= &omap3_am33xx_timer,
+	.init_machine	= pcm051lb_init,
+MACHINE_END
